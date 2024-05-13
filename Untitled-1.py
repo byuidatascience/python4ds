5b4c2c5af6940cbe89d2c48da91d3c7eb0ca1fd8
Chaz <clarkty@byui.edu>
2024-05-13 08:23:21 -0600

    updates

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+#%%
+print('hello world')
+# %%
+# Please type your favorite color: Hot Pink
+# Your favorite color is
+# Hot Pink
+color = input('Please type your favorite color: ')
+# %%
+# i need code that will ask the favorite color and then print it out
+def fav_color(color):
+    print(f'Your favorite color is \n{color}')
+#%%
+fav_color(color)
+# %%
